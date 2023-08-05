4fe675d7ed12b6c23d048a75195ca9a8997376c2
shivi-17june <srivastavdisha176@gmail.com>
2023-08-05 13:17:33 +0530

    personal information

---
+++ index.js
@@ -0,0 +1,16 @@
+function longestSubarrayWithSumAtMostk(nums,k){
+    let left=0;
+    let maxLength=0;
+    let currentSum=0;
+    for(let end=0; end<n ;end++){
+        currentsum += nums[end];
+        start++
+    }
+    maxLength = Math.max(maxLength, end-start+)
+;
+return maxLength;
+}
+const nums =p[];
+const k = 3;
+const output = tSubarraySumAtMostK(Nums, k);
+SVGSymbolElement.log(output);
